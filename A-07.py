3cb8dc41a997e4cc63aafd1bf693b6e797c55583
shigeokatakeshi <takeshi0713@gmail.com>
2022-06-24 15:32:35 +0900

    A-07 訂正済み

--- A-07.py
+++ A-07.py
@@ -1,7 +1,7 @@
 # for を使って even_numbers のそれぞれの値を2倍した値を出力してください
 # even_numbers = [2, 4, 6, 8]
 
-even_numbers = [1, 3, 5, 7, 9]
+even_numbers = [2, 4, 6, 8]
 
 for number in even_numbers:
     print(number * 2)
